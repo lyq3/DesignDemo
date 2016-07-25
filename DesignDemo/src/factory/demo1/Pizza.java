f2f05557a304cc63615a9c5b38173d5f3c9afa74
Mrluo <admin@9idk.com>
2016-07-25 16:09:00 +0800

    122

---
+++ DesignDemo/src/factory/demo1/Pizza.java
@@ -0,0 +1,33 @@
+package factory.demo1;
+
+/**
+ * 披萨类
+ * @author Mr.luo
+ *
+ */
+public class Pizza {
+	/**
+	 * 准备
+	 */
+	public void prepare(){
+		
+	}
+	/**
+	 * 烘焙
+	 */
+	public void bake(){
+		
+	}
+	/**
+	 * 切片
+	 */
+	public void cut(){
+		
+	}
+	/**
+	 * 装盒
+	 */
+	public void box(){
+		
+	}
+}
---
+++ DesignDemo/src/factory/demo1/PizzaStore.java
@@ -0,0 +1,27 @@
+package factory.demo1;
+
+/**
+ * 披萨店的父类，所有披萨店继承此类
+ * @author Mr.luo
+ *
+ */
+public abstract class PizzaStore {
+	
+	/**
+	 * 创建披萨，由各个披萨店自己决定
+	 * @param type
+	 * @return
+	 */
+	protected abstract Pizza createPizza(String type);
+	
+	public Pizza orderPizza(String type){
+		Pizza pizza;
+		pizza=createPizza(type);
+		pizza.prepare();
+		pizza.bake();
+		pizza.cut();
+		pizza.box();
+		return pizza;
+		
+	}
+}
